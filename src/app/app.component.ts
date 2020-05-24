e65203de1fcb7c1b0cfb2597ad360b36ea744a8d
ambrish <ambrish.java@gmail.com>
2020-05-24 15:28:31 +0530

    Wikipedia Search App - RxJS Basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,104 @@
-import { Component, Injectable, ReflectiveInjector } from '@angular/core';
-import { WikipediaService } from './wikipedia.service';
+import { Component } from '@angular/core';
+
+interface Passenger {
+  id: number;
+  fullname: string;
+  checkedIn: boolean;
+}
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['app.component.scss'],
+  template: `
+    <div class="app">
+      <input
+        type="text"
+        [value]="name"
+        (input)="handleChange($event.target.value)">
+
+      <ng-template [ngIf]="name.length > 2">
+        <div>
+          Searching for... {{ name }}
+        </div>
+      </ng-template>
+
+      <div *ngIf="name.length > 2">
+        Searching for... {{ name }}
+      </div>
+
+      <h3>Airline Passengers  using ng-template & [class] property</h3>
+      <ul>
+        <ng-template ngFor let-passenger let-i="index" [ngForOf]="passengers">
+          <li>
+            <span
+              class="status"
+              [class.checked-in]="passenger.checkedIn"></span>
+            {{ i }}: {{ passenger.fullname }}
+          </li>
+        </ng-template>
+      </ul>
+      <h3>Airline Passengers using *ngFor & [ngClass]</h3>
+      <ul>
+        <li *ngFor="let passenger of passengers; let i = index;">
+          <span
+            class="status"
+            [ngClass]="{
+              'checked-in': passenger.checkedIn,
+              'checked-out': !passenger.checkedIn
+            }"></span>
+          {{ i }}: {{ passenger.fullname }}
+        </li>
+      </ul>
+      <h3>Airline Passengers using [style]</h3>
+      <ul>
+        <li *ngFor="let passenger of passengers; let i = index;">
+          <span
+            class="status"
+            [style.backgroundColor]="(passenger.checkedIn ? '#2ecc71' : '#c0392b')"></span>
+          {{ i }}: {{ passenger.fullname }}
+        </li>
+      </ul>
+      <h3>Airline Passengers using [ngStyle]</h3>
+      <ul>
+        <li *ngFor="let passenger of passengers; let i = index;">
+          <span
+            class="status"
+            [ngStyle]="{
+              backgroundColor: (passenger.checkedIn ? '#2ecc71' : '#c0392b')
+            }"></span>
+          {{ i }}: {{ passenger.fullname }}
+        </li>
+      </ul>
+
+    </div>
+  `
 })
 export class AppComponent {
-  pages = [];
+  name = '';
 
-  constructor(private wikipedia: WikipediaService) {}
+  passengers: Passenger[] = [{
+    id: 1,
+    fullname: 'Stephen',
+    checkedIn: true
+  }, {
+    id: 2,
+    fullname: 'Rose',
+    checkedIn: false
+  }, {
+    id: 3,
+    fullname: 'James',
+    checkedIn: true
+  }, {
+    id: 4,
+    fullname: 'Louise',
+    checkedIn: true
+  }, {
+    id: 5,
+    fullname: 'Tina',
+    checkedIn: false
+  }];
 
-  onTerm(term: string) {
-    this.wikipedia.search(term).subscribe((pages) => {
-      this.pages = pages;
-    });
+  handleChange(value: string) {
+    this.name = value;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
 
-import { AppComponent } from './app.component';
-import { SearchBarComponent } from './search-bar/search-bar.component';
-import { PageListComponent } from './page-list/page-list.component';
+import {AppComponent} from './app.component';
+import {SearchBarComponent} from './search-bar/search-bar.component';
+import {PageListComponent} from './page-list/page-list.component';
+
+import {CommonModule} from '@angular/common';
+import { ProductsRxjsModule } from './products-rxjs/products-rxjs.module';
 
 @NgModule({
   declarations: [AppComponent, SearchBarComponent, PageListComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, CommonModule, ProductsRxjsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
