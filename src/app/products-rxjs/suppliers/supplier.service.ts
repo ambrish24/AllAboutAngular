882d7a772f9c3cb3ec764d3963f93245b77775d5
ambrish <ambrish.java@gmail.com>
2020-05-26 23:42:49 +0530

    Higher Order Observable - Nesting of Subscribe - Bad Practice

--- src/app/products-rxjs/suppliers/supplier.service.ts
+++ src/app/products-rxjs/suppliers/supplier.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { throwError } from 'rxjs';
+import {of, throwError} from 'rxjs';
+import {Supplier} from './supplier';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +11,22 @@ import { throwError } from 'rxjs';
 export class SupplierService {
   suppliersUrl = 'api/suppliers';
 
-  constructor(private http: HttpClient) { }
+  suppliersWithMap$ = of(1, 5, 8)
+    .pipe(
+      map(id => this.http.get<Supplier>(`${this.suppliersUrl}/${id}`)
+      )
+    );
+
+  // TODO : Nesting of Subscribe - Not a Good Practice but since inner subscription is also a
+  //  map we will have to perform nesting. Observable<Observable<Supplier>> - Observable of Observable of something
+  //  represents Higher Order Observable. We can use higher order RxJS operators in this case. We will have to
+  //  explicitly subscribe/unsubscribe as Async Pipe doesn't work with Higher Order Observable
+  constructor(private http: HttpClient) {
+    this.suppliersWithMap$
+      .subscribe(o => o.subscribe(
+        item => console.log('map result', item)
+      ));
+  }
 
   private handleError(err: any) {
     // in a real world app, we may send the server to some remote logging infrastructure
