28bd23c98dc52a865b3f7ef62417003be76ec8a6
ambrish <ambrish.java@gmail.com>
2020-05-27 07:24:22 +0530

    Combine multiple Async Pipes using combineLatest()

--- src/app/products-rxjs/products/product-list-alt/product-detail.component.ts
+++ src/app/products-rxjs/products/product-list-alt/product-detail.component.ts
@@ -3,7 +3,7 @@ import {ChangeDetectionStrategy, Component} from '@angular/core';
 import { ProductService } from '../product.service';
 import {Supplier} from '../../suppliers/supplier';
 import {catchError, filter, map} from 'rxjs/operators';
-import {combineLatest, EMPTY} from 'rxjs';
+import {combineLatest, EMPTY, Subject} from 'rxjs';
 import {Product} from '../product';
 
 @Component({
@@ -13,14 +13,15 @@ import {Product} from '../product';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProductDetailComponent {
-  errorMessage = '';
+  private errorMessageSubject = new Subject<string>();
+  errorMessage$ = this.errorMessageSubject.asObservable();
   product;
   productSuppliers: Supplier[] = [];
 
   product$ = this.productService.selectedProduct$
     .pipe(
       catchError(err => {
-        this.errorMessage = err;
+        this.errorMessageSubject.next(err);
         return EMPTY;
       })
     );
@@ -28,7 +29,7 @@ export class ProductDetailComponent {
   productSuppliers$ = this.productService.selectedProductSuppliers$
     .pipe(
       catchError(err => {
-        this.errorMessage = err;
+        this.errorMessageSubject.next(err);
         return EMPTY;
       })
     );
@@ -37,6 +38,17 @@ export class ProductDetailComponent {
     .pipe(
       map((p: Product) =>
         p ? `Product Detail for: ${p.productName}` : null)
+    );
+
+  vm$ = combineLatest([
+    this.product$,
+    this.productSuppliers$,
+    this.pageTitle$
+  ])
+    .pipe(
+      filter(([product]) => Boolean(product)),
+      map(([product, productSuppliers, pageTitle]) =>
+        ({ product, productSuppliers, pageTitle }))
     );
 
 
